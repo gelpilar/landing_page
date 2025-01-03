7b2da2d21f09d770981acd0ce7d82ef259cfe337
gelpilar <pilarscoll@gmail.com>
2025-01-03 10:45:49 -0300

    arreglo opiniones

--- app/reviews/page.jsx
+++ app/reviews/page.jsx
@@ -10,23 +10,36 @@ import React from 'react'
 const page = () => {
   return (
     <>
-    
-      <Opiniones></Opiniones>
-    
-    <div className='flex flex-column items-center w-screen mt-20 h-screen text-center 	'>
-    <div className='bg-indigo-100 h-3/4 w-2/4'>
-         <h1 className='text-indigo-950 pl-20 pt-20 text-3xl'>Testimonios</h1>
-         <p className='text-indigo-950 pl-20 pr-20 text-2xl'>Las opiniones de nuestros clientes siempre nos sorprenden y nos motivan a seguir creciendo. Cada comentario refleja el impacto positivo que nuestras soluciones tienen en sus negocios, y nos impulsa a seguir innovando para brindarles el mejor servicio. ¡Estamos orgullosos de ser parte del éxito de nuestros clientes!</p>
-         </div>
-         <div className='bg-transparent h-3/4 w-2/4 border-4'>
-         <iframe src="https://www.youtube.com/embed/19g66ezsKAg" allowFullScreen className='w-full h-full' />
-         </div>
-         
-      </div>
-      <Cta img={""} men={"Contactanos"} tit={"¿Tenes un problema? Le encontramos solucion"} url={`contact`} o={0.5}></Cta>
+  <Opiniones />
+
+  <div className="flex flex-col lg:flex-row items-center justify-center w-screen mt-10 lg:mt-20 h-auto text-center">
+ 
+    <div className="bg-indigo-100 p-8 lg:h-3/4 lg:w-2/4 w-full lg:rounded-l-lg">
+      <h1 className="text-indigo-950 text-2xl lg:text-3xl font-bold mb-6">Testimonios</h1>
+      <p className="text-indigo-950 text-lg lg:text-xl">
+        Las opiniones de nuestros clientes siempre nos sorprenden y nos motivan a seguir creciendo. Cada comentario refleja el impacto positivo que nuestras soluciones tienen en sus negocios, y nos impulsa a seguir innovando para brindarles el mejor servicio. ¡Estamos orgullosos de ser parte del éxito de nuestros clientes!
+      </p>
+    </div>
 
 
-    </>
+    <div className="bg-transparent lg:h-3/4 lg:w-2/4 w-full h-64 border-4 border-indigo-950 lg:rounded-r-lg mt-6 lg:mt-0">
+      <iframe
+        src="https://www.youtube.com/embed/19g66ezsKAg"
+        allowFullScreen
+        className="w-full h-full rounded-md"
+      />
+    </div>
+  </div>
+
+  <Cta
+    img={""}
+    men={"Contactanos"}
+    tit={"¿Tenes un problema? Le encontramos solución"}
+    url={`contact`}
+    o={0.5}
+  />
+</>
+
   )
 }
 
